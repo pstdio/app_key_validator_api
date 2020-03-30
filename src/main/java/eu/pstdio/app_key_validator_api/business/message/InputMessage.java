2767b197e3c8064c31b91adea6926cbb35f7715d
mbogudal <m.bogudal@linux.pl>
2020-03-30 10:35:45 +0200

    fix

--- src/main/java/eu/pstdio/app_key_validator_api/business/message/InputMessage.java
+++ src/main/java/eu/pstdio/app_key_validator_api/business/message/InputMessage.java
@@ -5,5 +5,8 @@ package eu.pstdio.app_key_validator_api.business.message;
  * @author mib
  */
 public interface InputMessage {
-    
+    String getUuid();
+    void setUuid(String uuid);
+    String getKey();
+    void setKey(String key);
 }
--- src/main/java/eu/pstdio/app_key_validator_api/business/message/OutputMessage.java
+++ src/main/java/eu/pstdio/app_key_validator_api/business/message/OutputMessage.java
@@ -5,5 +5,6 @@ package eu.pstdio.app_key_validator_api.business.message;
  * @author mib
  */
 public interface OutputMessage {
-    
+    void setValid(boolean valid);
+    boolean isValid();
 }
