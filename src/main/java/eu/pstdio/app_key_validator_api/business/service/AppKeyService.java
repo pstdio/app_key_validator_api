e2db684edd8fdd8e8a9e148bbf4373f1fc747ae7
mbogudal <m.bogudal@linux.pl>
2020-04-05 15:49:45 +0200

    refactor of service

--- src/main/java/eu/pstdio/app_key_validator_api/business/service/AppKeyService.java
+++ src/main/java/eu/pstdio/app_key_validator_api/business/service/AppKeyService.java
@@ -5,5 +5,5 @@ package eu.pstdio.app_key_validator_api.business.service;
  * @author mib
  */
 public interface AppKeyService {
-    String compare(String json);
+    String validate(String json);
 }
